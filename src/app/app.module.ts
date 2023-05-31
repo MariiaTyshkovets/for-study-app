396bfa8605519318631abf8bc93845473bc53c39
Mariia Tyshkovets <maria15021996@gmail.com>
2023-05-31 18:00:28 +0300

    basics and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    CommonModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
